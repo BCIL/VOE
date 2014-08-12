593fd066e4ad325fe507c976e9552c7854b5222f
Baekdoo Ryan Kim <baegi7942@hotmail.com>
2014-08-12 17:05:46 -0400

    pyloxml to json and draw the tree

---
+++ Tree_from_phyloxml_json/loadphyloXML.py
@@ -0,0 +1,16 @@
+from ete2 import Phyloxml
+project = Phyloxml()
+project.build_from_file("testTree.xml")
+
+# Each tree contains the same methods as a PhyloTree object
+for tree in project.get_phylogeny():
+    print tree
+    # you can even use rendering options
+    tree.show()
+    # PhyloXML features are stored in the phyloxml_clade attribute
+    for node in tree: 
+        print "Node name:", node.name
+        for seq in node.phyloxml_clade.get_sequence(): 
+            for domain in seq.domain_architecture.get_domain():
+                domain_data = [domain.valueOf_, domain.get_from(), domain.get_to()]
+                print "  Domain:", '\t'.join(map(str, domain_data))
