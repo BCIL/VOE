805c29db4edee0ec9910b45a3931976ccf7ea6d6
Baekdoo Ryan Kim <baegi7942@hotmail.com>
2014-10-16 13:36:29 -0400

    updated!

--- PhyloXML_Tree/Curr_build/uploadFile.php
+++ PhyloXML_Tree/Curr_build/uploadFile.php
@@ -1,25 +1,40 @@
 <?php
 
     $filename = $_FILES["Filedata"]["tmp_name"];
+    $filename1 = $_FILES["Filedata"]["name"];
     $filepath = "uploaded/".$filename;
-    $ok = move_uploaded_file($_FILES["Filedata"]["tmp_name"],
-                    "uploaded/" . $_FILES["Filedata"]["name"]);
+    $ext = explode(".", $filename1);
+    $ext = $ext[1];     # get extention info.
+    
+    #$extmsg = "extention? : ";
+    #debug_to_console ($extmsg, $ext);
+    
+    if ($ext == 'xml')  {
+        $ok = move_uploaded_file($_FILES["Filedata"]["tmp_name"],
+                        "uploaded/" . $_FILES["Filedata"]["name"]);
 
-    // This message will be passed to 'oncomplete' function
-    //echo "uploaded/" . $_FILES["Filedata"]["name"];
+        // This message will be passed to 'oncomplete' function
+        //echo "uploaded/" . $_FILES["Filedata"]["name"];
 
-    $var1 = "uploaded/" . $_FILES["Filedata"]["name"];
-        #echo "Variable check: " . $var1 . "<br>";
+        $var1 = "uploaded/" . $_FILES["Filedata"]["name"];
+            #echo "Variable check: " . $var1 . "<br>";
 
-        #/////////////////////////////////////////////////////////////////////
-        #//////// To excute the python script
-        $result = exec('python modifyXML.py ' . $var1);
+            #/////////////////////////////////////////////////////////////////////
+            #//////// To excute the python script
+            $result = exec('python modifyXML.py ' . $var1);
 
-        //echo "Excuted python?: " . $result;
-        $comment = "Excuted python?: ";
-        debug_to_console( $comment, $result);
+            //echo "Excuted python?: " . $result;
+            $comment = "Excuted python?: ";
+            debug_to_console( $comment, $result);
 
-        echo $_FILES["Filedata"]["name"];
+            echo $_FILES["Filedata"]["name"];
+    }
+    else {
+        $msg = "where? : ";
+        $val = "error section";
+        debug_to_console ($msg, $val);
+        echo "error";
+    }
 
     function debug_to_console( $data, $comment ) {
         if ( is_array( $data ) )
