70a8d858ef3e7752503cb00e39868cd0b3349738
Baekdoo Ryan Kim <baegi7942@hotmail.com>
2015-04-27 17:38:07 -0400

    BioITcore_website
    The template contains PhyloXML, Google Genomics, bed file
    visualizations.

---
+++ Website/BioITCore/js/ourWork.js
@@ -0,0 +1,6 @@
+
+// phyloXML
+	$('#phyloXML_main').attr('src','http://54.164.172.154');
+
+// Google Genomics
+	$('.googlegenomics_main').attr('src','http://biod3js.s3.amazonaws.com/BioITCore/GoogleGenomics/DataSets_References_7.html');
