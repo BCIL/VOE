7cdcbcea9d99ea64762c414b9e4f045056305a14
Baekdoo Ryan Kim <baegi7942@hotmail.com>
2015-10-08 20:10:02 -0400

    update

--- Archive/Website/Website/BioITCore/js/ourWork.js
+++
@@ -1,6 +0,0 @@
-
-// phyloXML
-	$('#phyloXML_main').attr('src','http://54.164.172.154');
-
-// Google Genomics
-	$('.googlegenomics_main').attr('src','http://biod3js.s3.amazonaws.com/BioITCore/GoogleGenomics/GoogleGenomics_main.html');
--- Archive/Website/Website/PhyloXML_Tree/Old_builds/build_0/execChk.php
+++
@@ -1,6 +0,0 @@
-<?php
-function exec_enabled() {
-  $disabled = explode(',', ini_get('disable_functions'));
-  return !in_array('exec', $disabled);
-}
-?>
